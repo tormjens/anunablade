8b8de9f09c283c7436267a817aca19099cf4d1f9
Tor Morten Jensen <tormorten@anunatak.no>
2015-06-12 15:58:11 +0200

    plugin require

--- lib/activation.php
+++ lib/activation.php
@@ -10,10 +10,6 @@ add_action( 'tgmpa_register', 'anuna_theme_register_required_plugins' );
 
 function anuna_theme_register_required_plugins() {
 
-    /**
-     * Array of plugin arrays. Required keys are name and slug.
-     * If the source is NOT from the .org repo, then source is also required.
-     */
     $plugins = array(
 
         array(
@@ -28,20 +24,13 @@ function anuna_theme_register_required_plugins() {
 
     );
 
-    /**
-     * Array of configuration settings. Amend each line as needed.
-     * If you want the default strings to be available under your own theme domain,
-     * leave the strings uncommented.
-     * Some of the strings are added into a sprintf, so see the comments at the
-     * end of each line for what each argument will be.
-     */
     $config = array(
         'default_path' => '',                      // Default absolute path to pre-packaged plugins.
-        'menu'         => 'tgmpa-install-plugins', // Menu slug.
+        'menu'         => 'anunastart-install-plugins', // Menu slug.
         'has_notices'  => true,                    // Show admin notices or not.
         'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
         'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
-        'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+        'is_automatic' => true,                   // Automatically activate plugins after installation or not.
         'message'      => '',                      // Message to output right before the plugins table.
         'strings'      => array(
             'page_title'                      => __( 'Install Required Plugins', 'roots' ),
