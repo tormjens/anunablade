141a95562fa26581a81710efe96442c826f90d74
Tor Morten Jensen <tormorten@anunatak.no>
2015-07-01 13:21:38 +0200

    add simple map styling

--- assets/dist/js/scripts.js
+++ assets/dist/js/scripts.js
@@ -8250,7 +8250,32 @@ var BrowserDetect = {
  * for more, see: http://www.advancedcustomfields.com/resources/google-map/
  */
 (function($) {
+
+	/**
+	 * An array containing the jQuery objects where maps should be inited
+	 */
+	var mapElems = [
+		$('.acf-map')
+	];
+
+	/**
+	 * Decides the colors of the map
+	 */
+	var colors = {
+		names			: '#000000',
+		landscape		: '#fafafa',
+		water			: '#aaaaaa',
+	};
+
+	/**
+	 * The maps object
+	 */
 	var maps = {
+		/**
+		 * Renders the map
+		 * @param  {Object} $el
+		 * @return {Object}
+		 */
 		render: function($el) {
 			var that = this;
 
@@ -8262,11 +8287,95 @@ var BrowserDetect = {
 				zoom		: 16,
 				center		: new google.maps.LatLng(0, 0),
 				mapTypeId	: google.maps.MapTypeId.ROADMAP,
-				styles 		: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
+				styles 		: [
+				    {
+				        "featureType":"administrative",
+				        "elementType":"labels.text.fill",
+				        "stylers":[
+				            {
+				                "color":colors.names
+				            }
+				        ]
+				    },
+				    {
+				        "featureType":"landscape",
+				        "elementType":"all",
+				        "stylers":[
+				            {
+				                "color":colors.landscape
+				            }
+				        ]
+				    },
+				    {
+				        "featureType":"poi",
+				        "elementType":"all",
+				        "stylers":[
+				            {
+				                "visibility":"off"
+				            }
+				        ]
+				    },
+				    {
+				        "featureType":"road",
+				        "elementType":"all",
+				        "stylers":[
+				            {
+				                "saturation":-100
+				            },
+				            {
+				                "lightness":45
+				            }
+				        ]
+				    },
+				    {
+				        "featureType":"road.highway",
+				        "elementType":"all",
+				        "stylers":[
+				            {
+				                "visibility":"simplified"
+				            }
+				        ]
+				    },
+				    {
+				        "featureType":"road.arterial",
+				        "elementType":"labels.icon",
+				        "stylers":[
+				            {
+				                "visibility":"off"
+				            }
+				        ]
+				    },
+				    {
+				        "featureType":"transit",
+				        "elementType":"all",
+				        "stylers":[
+				            {
+				                "visibility":"off"
+				            }
+				        ]
+				    },
+				    {
+				        "featureType":"water",
+				        "elementType":"all",
+				        "stylers":[
+				            {
+				                "color":colors.water
+				            },
+				            {
+				                "visibility":"on"
+				            }
+				        ]
+				    }
+				]
 			};
 
 			// add class
 			$el.addClass('map-inited');
+
+			// set height
+			if($el.height() === 0) {
+				$el.height(350);
+			}
 
 			// create map
 			var map = new google.maps.Map( $el[0], args);
@@ -8284,6 +8393,11 @@ var BrowserDetect = {
 
 			return map;
 		},
+		/**
+		 * Adds a marker to a map
+		 * @param {Object} $marker
+		 * @param {Object} map
+		 */
 		add_marker: function($marker, map) {
 			// var
 			var latlng = new google.maps.LatLng( $marker.attr('data-lat'), $marker.attr('data-lng') );
@@ -8310,7 +8424,15 @@ var BrowserDetect = {
 					infowindow.open( map, marker );
 				});
 			}
+
+			return map;
 		},
+		/**
+		 * Centers the map around all markers
+		 * @param  {Object} map
+		 * @param  {Object} $el
+		 * @return {Object}
+		 */
 		center_map: function(map, $el) {
 			// vars
 			var bounds = new google.maps.LatLngBounds();
@@ -8334,25 +8456,47 @@ var BrowserDetect = {
 
 			// add class
 			$el.addClass('map-centered');
+
+			return map;
 		}
 	};
 
+	/**
+	 * The function that runs when Google Maps is ready
+	 * @return {Void}
+	 */
 	window.readyMap = function() {
-		$('.acf-map').each(function(){
-			var map = maps.render( $(this) );
-			$(this).data('map', map);
+		$.each(mapElems, function(i, $el) {
+			$el.each(function(){
+				var map = maps.render( $(this) );
+				$(this).data('map', map);
+			});
 		});
 		$('html').removeClass('google-maps-loading').addClass('google-maps-ready');
 	};
 
+	/**
+	 * Loads up the Google Maps API
+	 * @return {Void}
+	 */
 	function loadScript() {
-	  	var script = document.createElement('script');
-	  	script.type = 'text/javascript';
-	  	script.src = 	'http://maps.google.com/maps/api/js?sensor=false&v=3.exp&callback=readyMap';
+	  	var script 		= document.createElement('script');
+	  	script.type 	= 'text/javascript';
+	  	script.src 		= 'http://maps.google.com/maps/api/js?sensor=false&v=3.exp&callback=readyMap';
 	  	document.body.appendChild(script);
 	}
 
-	if($('.acf-map').length !== 0) {
+	/**
+	 * Checks for presence of any of the map objects
+	 */
+	var present = 0;
+	$.each(mapElems, function(i, $el) {
+		if($el.length !== 0) {
+			present++;
+		}
+	});
+
+	if(present !== 0) {
 		window.onload = loadScript;
 		$('html').addClass('google-maps-loading');
 	}
