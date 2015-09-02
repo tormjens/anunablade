de8d588d0d313db67c9ec036131eaf1ec3d78e46
Tor Morten Jensen <tormorten@anunatak.no>
2015-09-02 12:47:50 +0200

    add livereload, chain methods and remove old minify files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var elixir = require('laravel-elixir'); // laravel elixir
 require('./tasks/elixir-vendor'); // the custom vendor task
+require('laravel-elixir-livereload');
 
 // where the unprocessed assets reside
 elixir.config.assetsPath = 'assets/src';
@@ -8,24 +9,30 @@ elixir.config.publicPath = 'assets/dist';
 // start elixir
 elixir(function(mix) {
 
-	// begin by creating vendor scripts
-	mix.vendor();
-
 	// copy jquery and modernizr
-	mix.copy('./bower_components/modernizr/modernizr.js', './assets/dist/js/modernizr.js');
-	mix.copy('./bower_components/jquery/dist/jquery.min.js', './assets/dist/js/jquery.min.js');
+	mix.copy('./bower_components/modernizr/modernizr.js', './assets/dist/js/modernizr.js')
+	   .copy('./bower_components/jquery/dist/jquery.min.js', './assets/dist/js/jquery.min.js');
 
-	// concatenate scripts
-	mix.scripts([
-		'vendor.js',
-		'plugins/*.js',
-		'_*.js'
-	], 'assets/dist/js/scripts.js');
+
+	// handle scripts
+	mix.vendor() // vendor scripts
+	   .scripts([ // concatenate scripts
+			'vendor.js',
+			'plugins/*.js',
+			'_*.js'
+		], 'assets/dist/js/scripts.js');
 
 	// run sass
 	mix.sass([
 		'./bower_components/foundation/scss/normalize.scss',
 		'app.scss'
 	], 'assets/dist/css/main.css');
+
+	// livereload
+	mix.livereload([
+		'./assets/dist/js/scripts.js',
+		'./assets/dist/css/main.css',
+		'./**/*.php'
+	]);
 
 });
